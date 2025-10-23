ace2656d162068446f9c55324893ab6062335a49
Ezequiel Alejandro Rodríguez Bracho <ezequielrodriguez1991@gmail.com>
2025-10-23 15:21:59 -0400

    fix: Change base path to root for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const base = command === 'serve' ? '/' : '/siempre-wifi/'
-  
-  return {
-    plugins: [react()],
-    base: base,
-    publicDir: 'public',
-    build: {
-      outDir: 'dist',
-      sourcemap: false,
-      assetsDir: 'assets',
+export default defineConfig({
+  plugins: [react()],
+  base: '/',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    server: {
-      port: 3000,
-      open: true,
-    },
-    resolve: {
-      alias: {
-        '@': '/src',
-      },
-    },
-  }
+  },
 })
